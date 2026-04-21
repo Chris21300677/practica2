29ef32823f61f42042f06f67d6d6d345568f7157
POC <a21300677@ceti.mx>
2026-04-20 22:43:04 -0600

    Integración con base de datos MySQL

--- src/app/servicios/products.ts
+++ src/app/servicios/products.ts
@@ -1,32 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Product } from '../modelos/product';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
-  private xmlUrl = 'productos.xml';
+  private apiUrl = 'http://localhost:3000/productos';
 
   constructor(private http: HttpClient) {}
 
   getProductos(): Observable<Product[]> {
-    return this.http.get(this.xmlUrl, { responseType: 'text' }).pipe(
-      map(xmlStr => {
-        const parser = new DOMParser();
-        const xml = parser.parseFromString(xmlStr, 'application/xml');
-        const nodos = xml.querySelectorAll('producto');
-        const productos: Product[] = [];
-        nodos.forEach(n => {
-          productos.push({
-            id: Number(n.querySelector('id')?.textContent),
-            nombre: n.querySelector('nombre')?.textContent || '',
-            precio: Number(n.querySelector('precio')?.textContent),
-            imagen: n.querySelector('imagen')?.textContent || ''
-          });
-        });
-        return productos;
-      })
-    );
+    return this.http.get<Product[]>(this.apiUrl);
   }
 }
